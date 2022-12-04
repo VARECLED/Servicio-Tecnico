c3c64e36ae2e10a27df1171a3c5807b81b8c13b3
Clemente Eduardo Vasquez Reyes <clementevasquez6@gmail.com>
2022-12-03 21:42:33 -0600

    Implementación Ajax
    Arreglo de error al implementar ajax, no funcionaba correctamente el llamado

--- js/funciones.js
+++ js/funciones.js
@@ -2,7 +2,7 @@ function fnAjax(url){
     var ajaxRequest = new XMLHttpRequest();
     ajaxRequest.onreadystatechange = function(){
         if(ajaxRequest.readyState==4 && ajaxRequest.status==200){
-            document.getElementById('page-content').innerHTML = ajaxRequest.responseText;
+            document.getElementById('imprimir').innerHTML = ajaxRequest.responseText;
         }        
     }    
     ajaxRequest.open("GET",url,true);
