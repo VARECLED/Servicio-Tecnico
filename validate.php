48cbeeb2ea47753df09b817b55eae008485e2886
Clemente Eduardo Vasquez Reyes <clementevasquez6@gmail.com>
2022-12-04 16:09:15 -0600

    PHP
    Conexión a la base de datos realizado, y validar inicio de sesión

---
+++ validate.php
@@ -0,0 +1,24 @@
+<?php
+    //Conexión a base de datos
+    include("config.php");
+    session_start();
+
+    $username = $_POST['usuario'];
+    $password = $_POST['clave'];
+
+    $username = $mysqli->real_escape_string($username);
+
+    $query = "SELECT Usuario, Contrasena FROM usuario WHERE Usuario = '$username' AND Contrasena = '$password';";
+    $result = $mysqli->query($query);
+
+    if($result->num_rows == 1){
+        $_SESSION['user']=$username;
+        header('Location: home.php'); 
+    }else{
+        echo '<script>';
+	    echo 'alert("USUARIO NO ENCONTRADO");';
+	    echo 'window.location="index.html";';
+	    echo '</script>';
+        //header('Location: index.html');
+    }
+?>
